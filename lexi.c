9903d143a5297d6824d4ddf40e4dbf3363e560b4
d4 r c k h <m4dd0x.xxx@gmail.com>
2020-09-05 12:58:24 +0300

    q to exit

--- lexi.c
+++ lexi.c
@@ -4,7 +4,6 @@ int main() {
     // store characters here
     char c;
     // read stdin
-    while (read(STDIN_FILENO, &c, 1) == 1);
-    // exit cleanly
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');    // exit cleanly
     return 0;
 }
