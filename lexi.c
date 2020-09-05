28db2cef711ee986ca2a4929281d37ea2b9acbc1
d4 r c k h <m4dd0x.xxx@gmail.com>
2020-09-05 13:05:50 +0300

    enable echoing back

--- lexi.c
+++ lexi.c
@@ -1,6 +1,30 @@
 #include <unistd.h>
+#include <termios.h>
+
+// struct for original terminal attrs
+struct termios orig_termios;
+
+
+void disableRawMode() {
+    // set old attrs
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+void enableRawMode() {
+    // get attrs
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    // set disableRawMode at exit
+    atexit(disableRawMode);
+    // create a copy of the attrs
+    struct termios raw = orig_termios;
+    // disable echo
+    raw.c_lflag &= ~(ECHO);
+    // save attrs
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+    // enable raw mode
+    enableRawMode();
     // store characters here
     char c;
     // read stdin
